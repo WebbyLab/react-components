0d1fd4c7cf474affc6da8195cae5a54dafeb6db3
Liubko <liubko.qwert@gmail.com>
2014-10-30 13:30:48 +0200

    Add onClick/onTouchTap to FaIcon

--- FaIcon.jsx
+++ FaIcon.jsx
@@ -4,16 +4,29 @@
 'use strict';
 
 var React = require('react');
+var cx = React.addons.classSet;
+
 require('font-awesome/css/font-awesome.min.css');
+require('./FaIcon.less');
 
 var FaIcon = React.createClass({
     propTypes: {
-        type: React.PropTypes.string.isRequired
+        type: React.PropTypes.string.isRequired,
+        onClick: React.PropTypes.func,
+        onTouchTap: React.PropTypes.func
     },
 
     render() {
+        var iconClass = cx({
+            'WLCFaIcon': true,
+            'fa': true,
+            'clickable': this.props.onClick || this.props.onTouchTap
+        });
+
         return (
-            <i className={'WLCFaIcon fa fa-'+this.props.type} />
+            <i className={iconClass + ' fa-' + this.props.type}
+                onClick={this.props.onClick}
+                onTouchTap={this.props.onTouchTap} />
         );
     }
 });
