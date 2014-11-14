4f0ef45871238e912ad76a457c321b489864f3ea
Liubko <liubko.qwert@gmail.com>
2014-11-14 17:08:29 +0200

    Add Image element

---
+++ Image.jsx
@@ -0,0 +1,41 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react');
+
+var Image = React.createClass({
+    propTypes: {
+        src: React.PropTypes.string,
+        placeholder: React.PropTypes.object // DOM ELEMENT
+    },
+
+    getDefaultProps() {
+        return {
+            placeholder: (<div />)
+        };
+    },
+
+    getInitialState() {
+        return {
+            src: this.props.src
+        };
+    },
+
+    handleImageLoadError() {
+        this.setState({
+            src: undefined
+        });
+    },
+
+    render() {
+        return this.state.src
+            ? <img className='Image'
+                   src={this.state.src}
+                   onError={this.handleImageLoadError} />
+            : this.props.placeholder;
+     }
+});
+
+module.exports = Image;
