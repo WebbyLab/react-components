ccf890677f9ab729efdc6ed1bd540b0dbb20148c
Alexey Zaslavsky <pterolex@yandex.ru>
2015-06-19 19:47:18 +0300

    Add more flags

--- Flag.jsx
+++ Flag.jsx
@@ -4,25 +4,48 @@
 'use strict';
 
 var React = require('react');
-var cx = React.addons.classSet;
+var cx = require('react-classset');
 
 require('./Flag.less');
 
 var Flag = React.createClass({
+
+    propTypes: {
+        // country: React.propTypes.string.isRequired
+    },
+
     render() {
         var country = this.props.country;
 
         var countryClass = cx({
             'Flag': true,
-            'DE': country === 'DE',
-            'FR': country === 'FR',
-            'ES': country === 'ES',
-            'IT': country === 'IT',
-            'UK': country === 'UK',
-            'US': country === 'US',
-            'CA': country === 'CA',
-            'NL': country === 'NL',
-            'CN': country === 'CN'
+            "US": country === "US",
+            "CA": country === "CA",
+            "MX": country === "MX",
+            "PE": country === "PE",
+            "AR": country === "AR",
+            "CL": country === "CL",
+            "CO": country === "CO",
+            "CR": country === "CR",
+            "GT": country === "GT",
+            "TT": country === "TT",
+            "UK": country === "UK",
+            "FR": country === "FR",
+            "NL": country === "NL",
+            "SE": country === "SE",
+            "FI": country === "FI",
+            "ES": country === "ES",
+            "IT": country === "IT",
+            "HU": country === "HU",
+            "PL": country === "PL",
+            "PT": country === "PT",
+            "GR": country === "GR",
+            "JP": country === "JP",
+            "TW": country === "TW",
+            "KR": country === "KR",
+            "TH": country === "TH",
+            "AU": country === "AU",
+            "NZ": country === "NZ"
         });
         return (
             <i className={countryClass}></i>
